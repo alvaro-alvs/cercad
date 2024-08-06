7097e7c751d65ec30a1703931161954fed4c63ab
Alvaro <alvaro.nrx88@gmail.com>
2024-08-06 09:32:33 -0300

    interface showcase

--- src/components/providers/PageProvider.tsx
+++ src/components/providers/PageProvider.tsx
@@ -2,7 +2,7 @@
 
 export default function PageProvider({ children }: { children: React.ReactNode }) {
     return (
-        <section className="min-w-screen w-full min-h-screen h-full flex flex-col items-center bg-gradient-to-br from-sky-300 via-teal-100 to-rose-200">
+        <section className="min-w-screen w-full min-h-screen h-full max-sm:w-fit flex flex-col items-center bg-gradient-to-br from-sky-300 via-teal-100 to-rose-200">
             {children}
         </section>
     )
